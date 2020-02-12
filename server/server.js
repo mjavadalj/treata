4f4457155f673229dab98ce3a2d9be747456477c
mjavad <mjavad.alj@yahoo.com>
2020-02-12 22:12:04 +0330

    .env

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
+require('dotenv').config({
+    path: './server/.env'
+})
+
 server.listen(port, () => {
     console.log(`server is UP on port : ${port}...`);
 });
