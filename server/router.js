a134a71f9dcb92e3310a02f011d73cd6ce12209e
mjavad <mjavad.alj@yahoo.com>
2020-02-21 10:23:40 +0330

    testi

--- server/router.js
+++ server/router.js
@@ -3,5 +3,11 @@ const userRoutes = require('./api/routes/userRoutes');
 const newsRoutes = require('./api/routes/newsRoutes');
 router.use('/user', userRoutes);
 router.use('/news', newsRoutes);
+router.use('/test', (req, res) => {
+    return res.status(200).json({
+        message: "ok",
+        text: req.body.text
+    })
+});
 
 module.exports = router;
