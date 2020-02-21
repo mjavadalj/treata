c72e79b955d4d5099db8cd80033d2ae7e045efcf
mjavad <mjavad.alj@yahoo.com>
2020-02-21 12:39:33 +0330

    get all news

--- server/api/routes/newsRoutes.js
+++ server/api/routes/newsRoutes.js
@@ -10,7 +10,7 @@ router.post('/removenews', logger.newsLogger, newsController.removeNews);// auth
 router.post('/uploadpics', logger.newsLogger, upload.array('photos', 3), newsController.uploadPic);// authentication.authenticated, authentication.isContentCreator,
 // router.post('/updatenews', logger.newsLogger, newsController.updateNews);
 router.post('/getnews', logger.newsLogger, newsController.getNews);
-router.post('/news', logger.newsLogger, newsController.getAllNews);
+router.get('/news', logger.newsLogger, newsController.getAllNews);
 
 router.post('/searchnews', logger.userLogger, newsController.searchNews);  /*authentication.authenticated, authentication.verfied, */
 module.exports = router;
